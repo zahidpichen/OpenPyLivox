ba7b99d172fb8d503d269f5f8106789f628de95a
zahidpichen <137002342+zahidpichen@users.noreply.github.com>
2025-05-08 21:02:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='openpylivox_pkg',
-    version='1.1.0',
-    url='https://github.com/ryan-brazeal-ufl/openpylivox',
-    author='Ryan Brazeal',
-    author_email='ryan.brazeal@ufl.edu',
+    name='openpylivox',
+    version='1.1.1',
+    url='https://github.com/zahidpichen/openpylivox',
+    author='Zahid Pichen',
+    author_email='zahidpichen1@gmail.com',
     packages=['openpylivox'],
     description='Python3 driver for Livox lidar sensors',    
     install_requires=['numpy >= 1.15.4', 'crcmod >= 1.7.0', 'deprecated >= 1.2.7', 'laspy >= 1.7.0', 'tqdm >= 4.48.0'],
